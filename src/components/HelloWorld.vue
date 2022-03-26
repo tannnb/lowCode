8d583333ceb9ee6ad97ea1cb22abf0058e98848f
tanbingword@163.com <tanbingwork@163.com>
2022-03-26 23:10:32 +0800

    test:测试demo学习

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,13 +2,18 @@
 <template>
   <div class="HelloWorld-container">
     <h1>HelloWorld大标题</h1>
-    <button>按钮</button>
+    <button @click="hanldeClick">{{btnRef}}</button>
+    <input type="text" v-model="todo">
+    <button class="addTodo" @click="addTodo">addTodo</button>
+    <ul>
+      <li v-for="(todo,index) in todos" :key="index">{{todo}}</li>
+    </ul>
     <Hello msg="123" />
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Hello from './Hello.vue'
 
 export default defineComponent({
@@ -16,6 +21,30 @@ export default defineComponent({
   components: { Hello },
   props: {
     msg: String
+  },
+  emits: ['send'],
+  setup (props, context) {
+    const btnRef = ref(100)
+    function hanldeClick () {
+      btnRef.value += 1
+    }
+
+    const todo = ref('')
+    const todos = ref([])
+    const addTodo = () => {
+      if (todo.value) {
+        todos.value.push(todo.value)
+        context.emit('send', todo.value)
+      }
+    }
+
+    return {
+      hanldeClick,
+      btnRef,
+      todo,
+      addTodo,
+      todos
+    }
   }
 })
 </script>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -14,13 +14,43 @@ import Hello from '@/components/Hello.vue'
 // 使用getComponent的意义
 // 不必须测试子组件里面的内容，只要判断知否渲染了子组件，是否传递了正确的属性就可以了
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('测试组件渲染与props数据传递:', () => {
     const msg = '这是新传入的信息'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
     // console.log(wrapper.get('h1').text())
-    // expect(wrapper.text()).toMatch(msg)
     console.log(wrapper.findAllComponents(Hello))
+  })
+
+  it('测试HelloWorld组件内按钮点击事件:', async () => {
+    const msg = '这是新传入的信息'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    // trigger为异步,需等到view更新后在断言
+    await wrapper.get('button').trigger('click')
+    console.log(wrapper.html())
+    expect(wrapper.get('button').text()).toBe('101')
+  })
+
+  it('测试输入框更改后,点击按钮，渲染时候时候更新:', async () => {
+    const msg = '这是新传入的信息'
+    const todoContent = '找到input元素调用setValue设置值'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    await wrapper.get('input').setValue(todoContent)
+    console.log('input当前设置的值:', wrapper.get('input').element.value)
+
+    expect(wrapper.get('input').element.value).toBe(todoContent)
+    await wrapper.get('.addTodo').trigger('click')
+    expect(wrapper.findAll('li')).toHaveLength(1) // 判断li的长度
+    expect(wrapper.get('li').text()).toBe(todoContent) // 判断第一个li的内容是否相等
+
+    // 验证点击按钮之后是否派发的emit='send'事件
+    expect(wrapper.emitted()).toHaveProperty('send')
+    const event = wrapper.emitted('send') || []
+    expect(event[0]).toEqual([todoContent])
   })
 })
